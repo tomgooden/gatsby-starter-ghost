adad3b338715310650ffceaf4d34696748d44e0f
Tom Gooden <tom@good.dog>
2019-12-10 13:06:01 -0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
     <Layout>
         <div className="container">
             <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
+                <h1 className="content-title">Darn 404</h1>
                 <section className="content-body">
                     Page not found, <Link to="/">return home</Link> to start over
                 </section>
